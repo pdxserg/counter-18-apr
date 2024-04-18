cb36b77beafe61b7ccdd06eadccf067f8d4cb721
SergeyStarchenko <pdxserg@gmail.com>
2024-04-18 11:21:53 -0700

    need red next

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	let [a, setA] = useState(0)
+
+	function onClickHandler() {
+		a = a + 1
+		setA(a)
+	}
+
+	function onClickHandlerZero() {
+		a = 0
+		setA(a)
+	}
+
+	return (
+
+		<div className="App-header">
+			<div className="c">
+				<div className="a">
+					<h2>{a}</h2>
+				</div>
+				<div className="b">
+					<button disabled={a === 5} onClick={onClickHandler}>inc</button>
+					<button disabled={a === 0} onClick={onClickHandlerZero}>reset</button>
+				</div>
+			</div>
+
+
+		</div>
+	);
 }
 
 export default App;
