1703f02974a8bcd65899da45ab40628ee3a01300
SergeyStarchenko <pdxserg@gmail.com>
2024-04-18 19:16:01 -0700

    start style

--- src/App.tsx
+++ src/App.tsx
@@ -9,15 +9,37 @@ function App() {
 	function onClickHandler() {a = a + 1; setA(a)}
 	function onClickHandlerZero() {a = 0; setA(a)}
 
+	const style1 = {
+		border: "1px solid #1df8dd",
+	padding: "10px 10px",
+	borderRadius: "5px"
+	}
+	const style2 = {
+		height: "60px",
+		display: "flex",
+		justifyContent: "center",
+		alignItems: "center",
+		borderRadius: "5px",
+		backgroundColor: "#1df8dd",
+		color: a === 5 ? "red" : "black"
+	}
+	const style3 = {
+		marginTop: "10px",
+	maxWidth: "400px",
+	border: "1px solid #1df8dd",
+	padding: "5px 10px",
+	borderRadius: "5px"
+	}
+
 
 	return (
 
 		<div className="App-header">
-			<div className="c">
-				<div className="a">
-					<h2>{a}</h2>
+			<div style={style1}>
+				<div style={style2}>
+					<h2 >{a}</h2>
 				</div>
-				<div className="b">
+				<div style={style3}>
 					<button disabled={a === 5} onClick={onClickHandler}>inc</button>
 					<button disabled={a === 0} onClick={onClickHandlerZero}>reset</button>
 				</div>
