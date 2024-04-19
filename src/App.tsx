22a97eccc921ac9b1e10005440cde7c655f437b1
SergeyStarchenko <pdxserg@gmail.com>
2024-04-18 19:48:41 -0700

    finished

--- src/App.tsx
+++ src/App.tsx
@@ -17,18 +17,20 @@ function App() {
 	}
 
 	const style1 = {
+		width: "120px",
 		border: "1px solid #1df8dd",
-		padding: "10px 10px",
+		padding: "5px 5px",
 		borderRadius: "5px"
 	}
 	const style2 = {
-		height: "60px",
+		height: "70px",
 		display: "flex",
 		justifyContent: "center",
 		alignItems: "center",
 		borderRadius: "5px",
 		backgroundColor: "#1df8dd",
-		color: a === 5 ? "red" : "black"
+		color: a === 5 ? "red" : "black",
+		fontSize: a === 5 ? "1.5rem" : "1rem"
 	}
 	const style3 = {
 		marginTop: "10px",
@@ -37,9 +39,6 @@ function App() {
 		padding: "5px 10px",
 		borderRadius: "5px"
 	}
-	const y = false
-	const z = true
-
 
 
 	return (
