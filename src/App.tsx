f963fd02823a98ba1a3714b37c4c99910239da37
SergeyStarchenko <pdxserg@gmail.com>
2024-04-19 09:24:06 -0700

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,33 @@
 import React, {useState} from 'react';
-
 import './App.css';
+
+
+const min_value = 0
+const max_value = 5
+
 
 function App() {
 
-	let [a, setA] = useState(0)
+	let [counter, setCounter] = useState(min_value)
 
 	function onClickHandler() {
-		a = a + 1;
-		setA(a)
+		setCounter(counter + 1)
 	}
 
 	function onClickResetHandler() {
-		a = 0;
-		setA(a)
+		setCounter(min_value)
 	}
-
-
-
-		//color: a === 5 ? "red" : "black",
-		//fontSize: a === 5 ? "1.5rem" : "1rem"
-
-
-
 
 	return (
 
-		<div className="App-header">
-			<div className={'c'}>
-				<div className={'a'}>
-					<h2>{a}</h2>
+		<div className="App">
+			<div className={'style1'}>
+				<div className={`style2  ${counter === max_value ? 'styleRed' : ''}`}>
+					<h2>{counter}</h2>
 				</div>
-				<div className={'b'}>
-					<button disabled={a === 5} onClick={onClickHandler}>inc</button>
-					<button disabled={a === 0} onClick={onClickResetHandler}>reset</button>
+				<div className={'style3'}>
+					<button disabled={counter === max_value} onClick={onClickHandler}>inc</button>
+					<button disabled={counter === min_value} onClick={onClickResetHandler}>reset</button>
 				</div>
 			</div>
 
